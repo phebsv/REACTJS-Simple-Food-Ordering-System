a66dbfd7b72c31daae94468a30f977a227547422
phebsv <phebsvillaflor@gmail.com>
2026-05-01 02:27:38 +0800

    updated router

--- food-ordering-system/src/main.tsx
+++ food-ordering-system/src/main.tsx
@@ -1,17 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-import { AdminProvider } from "./context/AdminContext";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-   <React.StrictMode>
-    <BrowserRouter>
-      <AdminProvider>
-        <App />
-      </AdminProvider>
-    </BrowserRouter>
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>
 );
